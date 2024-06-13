74c14aa566e90bae0e7a2a2049087c93390ac477
abel-all <abel-all@student.1337.ma>
2024-06-14 00:15:29 +0100

    add some change on home page v2

--- Frontend/src/pages/Home/home_components/HomeContent.jsx
+++ Frontend/src/pages/Home/home_components/HomeContent.jsx
@@ -4,8 +4,10 @@ function HomeContent() {
 
     return (
         <div className="mt-[200px] flex flex-col gap-[30px] items-center w-full">
-            <div className="text-[64px] max-sm:text-[31px] font-bold text-white">Play <span className="text-[#00CEFF] font-extrabold">Ping Pong</span> Anytime, Anywhere</div>
-            <p className="opacity-90 text-[#EEEEEE]">
+            <div className="text-[64px] max-md:text-[40px] max-sm:text-[31px] font-bold text-white text-center max-w-[450px]">
+                Play <span className="text-[#00CEFF] font-extrabold">Ping Pong</span> Anytime, Anywhere
+            </div>
+            <p className="opacity-90 text-[#EEEEEE] text-center max-sm:text-[13px] max-w-[450px]">
                 Ready to elevate your ping pong game? Compete in matches and tournaments on our platform to sharpen your skills and track your progress.
             </p>
             <Button width="max-w-[205px] w-full" title="Get Started" />

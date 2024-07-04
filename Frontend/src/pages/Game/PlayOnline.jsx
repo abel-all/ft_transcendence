7174131d4a55a53b2b56b18e6b7cf4f491203006
abdessamad el allali <abel-all@1337.com>
2024-07-04 18:17:20 +0100

    begin game page

--- Frontend/src/pages/Game/PlayOnline.jsx
+++ Frontend/src/pages/Game/PlayOnline.jsx
@@ -21,15 +21,12 @@ const PlayOnline = () => {
     return (
         <div className="h-[100vh] container mx-auto px-[10px]">
             <Header title="Online Game" activeSection="GametableIcon" />
-            {/* <div className="w-full max-w-[900px] h-full max-h-[600px] bg-[#1F1F1F] border border-[#626262] rounded-[13px] ">
-
-            </div> */}
-            {/* <MatchMaking /> */}
-            <div className="w-full h-[calc(100%-105px)] min-h-[1000px]  pb-[200px] flex flex-col justify-center items-center">
+            <MatchMaking />
+            {/* <div className="w-full h-[calc(100%-105px)] min-h-[1000px]  pb-[200px] flex flex-col justify-center items-center">
                 <canvas id="game-canva" width={900} height={600} className="bg-[#1F1F1F] border border-[#626262] rounded-[13px]">
 
                 </canvas>
-            </div>
+            </div> */}
             <BottomNaveBar activeSection="GametableIcon" />
         </div>
     )

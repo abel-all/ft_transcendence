1cd52a19b70d6aeb8d8a7f8365ff14e46293d664
abel-all <abel-all@student.1337.ma>
2024-07-07 12:52:19 +0100

    last update of game page

--- Frontend/src/pages/Game/MatchMakingCard.jsx
+++ Frontend/src/pages/Game/MatchMakingCard.jsx
@@ -18,7 +18,7 @@ const MatchMakingCard = (props) => {
         <div className="flex flex-col gap-[5px] w-[130px]">
             <div className="font-medium text-[25px] text-[#f0f0f1] w-full text-center">{props.username}</div>
             <div className={bg + " w-full h-[150px] relative rounded-[10px] hover:scale-[1.02] duration-[600ms] flex flex-col items-center"}>
-                <img className={"w-full max-w-[130px] h-[130px] border-black rounded-[10px] " + border} src={image} alt="profile image" />
+                <img className={"w-full max-w-[130px] h-[130px] border-black rounded-t-[10px] " + border} src={image} alt="profile image" />
                 <div className={"spiner w-full max-w-[17px] h-[17px] bg-transparent rounded-full " + hideSpiner} ></div>
             </div>
             <div className="w-full font-medium opacity-80 text-[#f0f0f1] flex gap-[15px]">
--- Frontend/src/pages/Game/PlayOnline.jsx
+++ Frontend/src/pages/Game/PlayOnline.jsx
@@ -1,32 +1,57 @@
-import MatchMaking from "./MatchMaking.jsx";
-import { useEffect, useState } from "react"
+// import MatchMaking from "./MatchMaking.jsx";
+import { useEffect, useRef, useState } from "react"
 import LoaderOntop from "../../components/LoaderOntop.jsx";
 import Header from "../../components/Header.jsx"
 import BottomNaveBar from "../../components/BottomNavBar.jsx"
 
 
 const PlayOnline = () => {
-
+    
     const [isLoaded, setIsLoaded] = useState(true);
+    const ref = useRef();
+    
+    const draw = () => {
+        
+        if(ref.current) {
+            const canvas = ref.current;
+            const context = canvas.getContext("2d");
+
+            context.shadowColor = 'transparent';
+            context.shadowBlur = 0;
+            context.shadowOffsetX = 0;
+            context.shadowOffsetY = 0;
+
+            context.fillStyle = 'white';
+            context.strokeStyle = 'red';
+            context.fillRect(0, 120,5, 25);
+            // context.strokeRect(0, 120,5, 25);
+            // context.fillRect(0, 0,5, 25)
+        }
+    }
 
     useEffect(() => {
         setTimeout(() => {
             setIsLoaded(false)
         }, 500);
+
     }, [])
+
+    useEffect(() => {
+        if (!isLoaded)
+            draw();
+    }, [isLoaded])
 
     if (isLoaded)
         return <LoaderOntop />
 
+    
+
     return (
         <div className="h-[100vh] container mx-auto px-[10px]">
             <Header title="Online Game" activeSection="GametableIcon" />
-            {/* <div className="w-full max-w-[900px] h-full max-h-[600px] bg-[#1F1F1F] border border-[#626262] rounded-[13px] ">
-
-            </div> */}
             {/* <MatchMaking /> */}
-            <div className="w-full h-[calc(100%-105px)] min-h-[1000px]  pb-[200px] flex flex-col justify-center items-center">
-                <canvas id="game-canva" width={900} height={600} className="bg-[#1F1F1F] border border-[#626262] rounded-[13px]">
+            <div className="w-full sm:h-[calc(100%-105px)] sm:flex sm:flex-col sm:justify-center sm:items-center">
+                <canvas ref={ref} id="game-canva" className="w-full max-w-[900px] h-[600px] bg-[#1F1F1F] border border-[#626262] rounded-[13px]">
 
                 </canvas>
             </div>

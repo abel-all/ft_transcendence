156ad0ca0c07916050b8180ef03f7662a2ccc755
Chahbi Youssef <116182776+y-chahbi@users.noreply.github.com>
2024-07-16 22:28:52 +0100

    Update ChatBottom.jsx

--- Frontend/src/pages/Chat/Chat_comp/ChatBottom.jsx
+++ Frontend/src/pages/Chat/Chat_comp/ChatBottom.jsx
@@ -1,57 +1,59 @@
-import send from "../../../assets/imgs/chat/send-02.svg"
-import microphone from "../../../assets/imgs/chat/microphone-02.svg"
-import camera from "../../../assets/imgs/chat/camera-02.svg"
-import paperclip from "../../../assets/imgs/chat/paperclip-01.svg"
-import { useRef, useState, useContext } from "react"
-import { sendMessageContext } from "./ChatSide"
-import { flushSync } from 'react-dom';
+import Chat from '../../../assets/Chat.json'
+import seen from '../../../assets/imgs/chat/seen.svg'
+import notseen from '../../../assets/imgs/chat/notyet.svg'
+import waithing from '../../../assets/imgs/chat/msg_waiting.svg'
+import failed from '../../../assets/imgs/chat/msg_failed.svg'
+import { useContext, useEffect, useState } from 'react'
+import { sendMessageContext } from './ChatSide'
 
 
-function ChatBottom() {
+
+
+function Messages(Data) {
 
     const messageContext = useContext(sendMessageContext);
-    const [textValue, setTextValue] = useState('');
-    const textAreaRef = useRef(null);
+    let userStyle;
+    
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            messageContext.goToButtom("auto")
+        }, 100)
 
-    const HandelSendMessage = (e) => {
-        if (messageContext.userAbleToSendMessage){
-            flushSync(() => {
-                messageContext.addMessage(textValue);
-                if (textAreaRef.current)
-                    textAreaRef.current.value = "";
-            });
-            setTimeout(() => {
-                messageContext.goToButtom("smooth");
-            }, 0)
-        }
-    }
+        return (() => {
+            clearTimeout(timer)
+        })
+    }, [])
 
-    const updateTextalue = (e) => {
-        setTextValue(e.target.value);
-    }
-
-    const handelEntry = (e) => {
-        if (e.key == "Enter")
-            HandelSendMessage();
-    }
+    const {messages} = useContext(sendMessageContext);
 
     return (
-        <>
-            <div className={`SendMessagePart flex flex-row justify-center items-center border-t-[1px] border-[#626262] h-[86px]`}>
-                <input autoComplete="off" id='txtarea' onKeyUp={handelEntry} ref={textAreaRef} onChange={updateTextalue}
-                    className="resize-none text-[20px] p-[27px] ml-[7px] grow opacity-80 placeholder:opacity-80 text-white
-                        placeholder:text-white bg-transparent h-full placeholder:font-[200] font-[200]
-                        placeholder:font-[Outfit] font-[Outfit] focus-visible:outline-none"
-                    placeholder="Type Something..."/>
-                <div className="SendMessage cursor-pointer flex flex-row p-[10px] items-center justify-center h-full border-l-[1px] border-[#626262]"
-                    onClick={HandelSendMessage}>
-                    <div className="text-[#00CEFF] text-[20px] font-[500] font-[Outfit]">Send</div>
-                    <img className="w-[30px] h-[30px]" src={send} alt=""/>
-                </div>
-            </div>
-        </>
+        <div ref={messageContext.messagesRef} className={"" + (Data.className) ? Data.className : ''}>
+            {
+                messageContext.messages.messages.map(chatMessages => {
+                    {(chatMessages.sender == "User1") ? userStyle = "text-white bg-[#0A0C0E] rounded-tr-lg " : userStyle = "text-black bg-white self-end rounded-tl-lg "}
+                    return (
+                        <div key={chatMessages.message_id} className={userStyle + 'break-all relative flex rounded-b-lg w-[70%] lg:w-[450px] m-[10px] p-[20px]'}>
+                            <p className='pb-[15px]'>{chatMessages.message}</p>
+                            {(chatMessages.sender == "User2") ?
+                                <>
+                                    <span className='absolute right-[45px] bottom-[5px]'>{chatMessages.timestamp}</span>
+                                    <img
+                                        src={
+                                            chatMessages.seen ? seen : (chatMessages.depands ? ((chatMessages.depands == "failed") ? failed : waithing) : notseen)}
+                                        alt='' className='absolute right-[15px] bottom-[5px]'/>
+                                </>
+                                : 
+                                    <span className='absolute right-[15px] bottom-[5px]'>{chatMessages.timestamp}</span>
+                            }
+                        </div>
+                    )
+                })
+            }
+        </div>
+
+
     )
 }
 
 
-export default ChatBottom
+export default Messages

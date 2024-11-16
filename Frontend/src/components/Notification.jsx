9ccbf1baab71e472b5a70243b57f9da520adaede
abel-all <abel-all@student.1337.ma>
2024-11-16 15:42:45 +0100

    add test tourn reminder

--- Frontend/src/components/Notification.jsx
+++ Frontend/src/components/Notification.jsx
@@ -16,8 +16,7 @@ let onetime = false;
 const Notification = () => {
   const notification = useAuth();
   const targetRef = useRef(null)
-  const [isLoaded, setIsLoaded] = useState(false)
-  const [isLoading, setIsLoading] = useState(true)
+  const [isLoaded, setIsLoaded] = useState(true)
   const [isZeroNotification, setIsZeroNotification] = useState(false)
   const [isBottomCounter, setIsBottomCounter] = useState(0)
   const [notiData, setNotiData] = useState([])
@@ -37,11 +36,10 @@ const Notification = () => {
         }
       )
         .then((response) => {
-          response?.data.length === 0
+          response?.data.length === 0 && isBottomCounter === 0
             ? setIsZeroNotification(true)
             : setNotiData((prev) => [...prev, ...response.data])
           setIsLoaded(false)
-          setIsLoading(false)
         })
         .catch((err) => {
           if (err.response?.status === 403) {
@@ -61,6 +59,10 @@ const Notification = () => {
   }, [isBottomCounter])
 
   useEffect(() => {
+    console.log("value of isLoaded is : ", isLoaded)
+  }, [isLoaded])
+
+  useEffect(() => {
     const currentRef = targetRef.current
 
     let isFetching = false
@@ -70,7 +72,9 @@ const Notification = () => {
       if (currentRef && !isFetching) {
         const { scrollTop, scrollHeight, clientHeight } = currentRef
         if (scrollTop + clientHeight >= scrollHeight) {
+          onetime = false
           isFetching = true
+          console.log("hello from handle scroll", )
           setIsLoaded(true)
           setCounter = setTimeout(() => {
             setIsBottomCounter((prev) => prev + 1)
@@ -85,7 +89,7 @@ const Notification = () => {
       
       return () => {
         if (currentRef) currentRef.removeEventListener('scroll', handleScroll)
-          clearTimeout(setCounter)
+        clearTimeout(setCounter)
     }
   }, [])
 
@@ -125,15 +129,13 @@ const Notification = () => {
       ref={targetRef}
       className="flex flex-col items-center flex-nowrap gap-[10px] max-md:pb-[100px] p-[10px] max-md:pr-[25px] w-[400px] h-[600px] max-md:w-[100vw] max-md:h-[100vh] overflow-y-scroll scrollbar-w bg-[#393e42] absolute max-md:fixed right-[5px] top-[53px] max-md:top-0 max-md:right-0 max-md:left-0 md:rounded-[15px] md:rounded-tr-[0]"
     >
-      {isLoading && <Spiner height="h-full" />}
-      {isZeroNotification && isBottomCounter === 0 ? (
+      {isZeroNotification ? (
         <div className="text-[#fff6f9] text-[20px] flex flex-col justify-center  h-full">
           No Notifications
         </div>
       ) : (
         notiData.map(({ content, notification_type, from_user }, index) => (
           <div
-          data
           key={index}
           data-id={notification_type}
           data-id1={from_user}
@@ -146,8 +148,7 @@ const Notification = () => {
           </div>
         ))
       )}
-
-      {isLoaded && <Spiner />}
+      {/* {isLoaded && <Spiner />} */}
     </div>
   )
 }
--- Frontend/src/pages/Game/CreateTournament.jsx
+++ Frontend/src/pages/Game/CreateTournament.jsx
@@ -4,6 +4,7 @@ import tourCreate from "../../assets/imgs/tourCreate.svg";
 import { useGameSettings } from "./GameSettingsContext";
 import CreateTournamentSection from "./CreateTournamentSection";
 import Spiner from "./Spiner";
+import Axios from "axios";
 
 const TournamentCard = ({
   image,
@@ -99,6 +100,26 @@ const CreateTournament = () => {
     gameContext.joinTour,
   ]);
 
+  const handleReminderClick = async () => {
+    let participantsNames = ["aaa", "bbb", "ccc", "ddd"];
+
+    await Axios.post(
+      "http://localhost:8800/api/profile/notification/tournament-reminder/",
+      {
+        usernames: participantsNames,
+      },
+      {
+        withCredentials: true,
+      }
+    )
+      .then((response) => {
+        console.log("data of friends is ", response?.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+};
+
   return (
     <div className="h-[calc(100vh-65px)] min-h-[1300px] py-[200px] w-full flex max-md:flex-col justify-center items-center gap-[10px]">
       {!createTour && !joinTour && (
@@ -115,6 +136,7 @@ const CreateTournament = () => {
               paramvalue={paramValue}
             />
           ))}
+          <button className="text-[#000] bg-[#fff] p-3 rounded-xl" onClick={handleReminderClick}>Tourn Reminder for testing!</button>
         </>
       )}
       {gameContext.loading && <Spiner height="h-full" />}
